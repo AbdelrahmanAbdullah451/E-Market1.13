999732ea608ae480a8d4f572d71fa392308f5410
Abdelrhman abduulah <abdonilsh20@fci.helwan.edu.eg>
2020-06-08 16:50:20 +0200

    upload

---
+++ Models/ProductCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Market.Models
+{
+    public class ProductCart
+    {
+        public Cart cart { get; set; }
+        public Product product { get; set; }
+    }
+}
---
+++ ViewModel/ProductCartVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using E_Market.Models;
+using E_Market.ViewModel;
+namespace E_Market.ViewModel
+{
+    public class ProductCartVM
+    {
+        public List<Product> products { get; set; }
+        public List<Cart> carts { get; set; }
+    }
+}
